8cd486079a27e0416c55f2438ee0790c072c7c8b
AaravSrivastav <22asrivastav@athenian.org>
2020-09-14 02:01:14 +0000

    Added the math methods to main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,14 +4,26 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double n = 10;
+        double m = (n + x/n)/2;
+        while(Math.abs(m-n)>.00001)
+        {
+            n = m;
+            m = (n + x/n)/2;
+        }
+
+        return m;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
         // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int fact = 1;
+        for(int i = x; i>1; i--)
+        {
+            fact*=i;
+        }
+        return fact;
     }
 
     // Calculates the value for the math constant e iteratively
@@ -19,17 +31,26 @@ public class MyMain {
     // different from the value Math.E
     public static double calculateE() {
         // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int i=0;
+        double E = 0;
+        while(Math.abs(E - Math.E)>0.00000000001)
+        {
+            E += 1/factorial(i);
+        }
+        return E;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // YOUR CODE HERE
-
-
-        
+        System.out.println("Enter a positive double");
+        double a = scan.nextDouble();
+        System.out.println(babylonian(a));
+        System.out.println("Enter a positive int");
+        int b = scan.nextInt();
+        System.out.println(factorial(b));
+        System.out.println(calculateE());        
 
         scan.close();
     }
